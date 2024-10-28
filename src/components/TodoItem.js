0339e534f9104dbfc3892f06a697759174d74b21
Deepak <138435129+deepakshah23@users.noreply.github.com>
2024-10-28 19:52:48 +0530

    Update TodoItem.js

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,7 +4,7 @@ import './todo.css'
 const TodoItem = ({ todo }) => {
   return (
     <li>
-      <img src="icon-192x192.png" loading="lazy" className='todoImg'/>
+      <img src="icon-192x192.png" alt="loding" className='todoImg'/>
       <span>{todo}</span>
     </li>
   );
